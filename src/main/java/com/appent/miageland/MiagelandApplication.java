bfbdb97e50e1f01d68d055807be00c8719efd0df
leoBazot <leo.bazot@protonmail.com>
2023-05-24 16:12:08 +0200

    Init spring project

---
+++ src/main/java/com/appent/miageland/MiagelandApplication.java
@@ -0,0 +1,13 @@
+package com.appent.miageland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiagelandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiagelandApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/appent/miageland/MiagelandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appent.miageland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiagelandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
